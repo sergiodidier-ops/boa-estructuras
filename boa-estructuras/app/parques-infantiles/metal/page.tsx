76d01bab284cdd803ad13ff1c1573d8548b62d4c
Sergio Fraile <sergio.fraile@boaestructuras.es>
2026-05-16 16:51:54 +0200

    Añadir modelo Ksetas

--- boa-estructuras/app/parques-infantiles/metal/page.tsx
+++ boa-estructuras/app/parques-infantiles/metal/page.tsx
@@ -10,9 +10,9 @@ const modelos = [
     enlace: "/parques-infantiles/metal/mod-torreroko",
   },
   {
-    nombre: "Torreroko",
-    imagen: "/fotos/metal/mod-torreroko/portada.png",
-    enlace: "/parques-infantiles/metal/mod-torreroko",
+   nombre: "Ksetas",
+  imagen: "/fotos/metal/mod-ksetas/portada.png",
+  enlace: "/parques-infantiles/metal/mod-ksetas",
   },
 ]
 
